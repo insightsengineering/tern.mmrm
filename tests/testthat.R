e3a88c8c39a6b239c4565ef2263ae175f3781645
Joe Zhu <sha.joe.zhu@gmail.com>
2022-03-07 15:21:04 +0800

    migrating files, init build [skip actions] (#4)

---
+++ tests/testthat.R
@@ -0,0 +1,10 @@
+pkg_name <- "tern.mmrm"
+if (requireNamespace("testthat", quietly = TRUE)) {
+  library(testthat)
+  reporter <- testthat::MultiReporter$new(list(
+    testthat::CheckReporter$new(),
+    testthat::JunitReporter$new(file = "junit-result.xml")
+  ))
+  test_results <- testthat::test_check(pkg_name, reporter = reporter)
+  saveRDS(test_results, "unit_testing_results.rds")
+}
