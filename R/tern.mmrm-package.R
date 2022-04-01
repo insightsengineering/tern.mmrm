3a17a88883ef5dc7f32a003cf6febd057dba4b65
Nikolas Burkoff <nikolas.burkoff@tessella.com>
2022-04-01 09:25:34 +0100

    add vignette

--- R/tern.mmrm-package.R
+++ R/tern.mmrm-package.R
@@ -12,3 +12,19 @@
 #' @importFrom dfoptim nmk
 #' @importFrom tern f_conf_level
 NULL
+
+#' Example dataset for `tern.mmrm` package.
+#'
+#' Measurements of FEV1 (forced expired volume in one second) is a measure of how quickly the lungs can be emptied.
+#' Low levels of FEV1 may indicate chronic obstructive pulmonary disease (COPD).
+#' @format A `tibble` with 800 rows and 7 variables:
+#' \describe{
+#'   \item{`USUBJID`}{price, in US dollars}
+#'   \item{`AVISIT`}{visit number}
+#'   \item{`ARMCD`}{treatment, `TRT` or `PBO`}
+#'   \item{`RACE`}{3-category race}
+#'   \item{`SEX`}{sex}
+#'   \item{`FEV1_BL`}{FEV1 at baseline (%)}
+#'   \item{`FEV1`}{FEV1 at study visits}
+#'}
+"mmrm_test_data"
