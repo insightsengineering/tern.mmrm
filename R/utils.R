d14e260c3ceb7e575f17e635249e815db1132bf0
Nikolas Burkoff <nikolas.burkoff@tessella.com>
2022-04-14 11:23:46 +0100

    linter

--- R/utils.R
+++ R/utils.R
@@ -1,15 +1,15 @@
 ## adapted from purrr, but we do not need the output saved
 capture_ouput <- function(code) {
   warnings <- character()
-  wHandler <- function(w) {
+  w_handler <- function(w) {
     warnings <<- c(warnings, w$message)
     invokeRestart("muffleWarning")
   }
   messages <- character()
-  mHandler <- function(m) {
+  m_handler <- function(m) {
     messages <<- c(messages, m$message)
     invokeRestart("muffleMessage")
   }
-  invisible(utils::capture.output(result <- withCallingHandlers(code, warning = wHandler, message = mHandler)))
+  invisible(utils::capture.output(result <- withCallingHandlers(code, warning = w_handler, message = m_handler)))
   list(result = result, warnings = warnings, messages = messages)
 }
