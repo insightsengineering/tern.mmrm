fa0e2ad359ea16dd1ec4bc27d5d7f6daea369822
Nikolas Burkoff <nikolas.burkoff@tessella.com>
2022-04-14 11:04:50 +0100

    remove purrr::quietly

---
+++ R/utils.R
@@ -0,0 +1,15 @@
+## adapted from purrr, but we do not need the output saved
+capture_ouput <- function(code) {
+  warnings <- character()
+  wHandler <- function(w) {
+    warnings <<- c(warnings, w$message)
+    invokeRestart("muffleWarning")
+  }
+  messages <- character()
+  mHandler <- function(m) {
+    messages <<- c(messages, m$message)
+    invokeRestart("muffleMessage")
+  }
+  invisible(capture.output(result <- withCallingHandlers(code, warning = wHandler, message = mHandler)))
+  list(result = result, warnings = warnings, messages = messages)
+}
