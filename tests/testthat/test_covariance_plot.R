d0de8ed4b95f8a3d9bb0f9061d3955f696a011b9
juliadedic1 <julia.dedic@roche.com>
2022-06-24 17:15:08 -0400

    style test_covariance file

--- tests/testthat/test_covariance_plot.R
+++ tests/testthat/test_covariance_plot.R
@@ -3,8 +3,10 @@ test_that("h_get_timepoint_vars works as expected with string specified", {
   data_wide <- dcast(mmrm_test_data, USUBJID + ARMCD ~ AVISIT, value.var = "FEV1")
   data_cov <- cov(data_wide[, 3:ncol(data_wide)], use = "pairwise.complete.obs", method = "pearson")
   result <- h_get_timepoint_vars(vcov_matrix = data_cov, string = "VIS")
-  expected <- list(numeric_vcov_matrix_row_label = c(1, 1, 2, 1, 2, 3, 1, 2, 3, 4),
-                   numeric_vcov_matrix_col_label = c(1, 2, 2, 3, 3, 3, 4, 4, 4, 4))
+  expected <- list(
+    numeric_vcov_matrix_row_label = c(1, 1, 2, 1, 2, 3, 1, 2, 3, 4),
+    numeric_vcov_matrix_col_label = c(1, 2, 2, 3, 3, 3, 4, 4, 4, 4)
+  )
   expect_identical(result, expected)
 })
 
@@ -22,7 +24,9 @@ test_that("h_get_timepoint_vars works as expected with string argument not neede
   row.names(data_cov) <- c(1, 2, 3, 4)
   colnames(data_cov) <- c(1, 2, 3, 4)
   result <- h_get_timepoint_vars(vcov_matrix = data_cov)
-  expected <- list(numeric_vcov_matrix_row_label = c(1, 1, 2, 1, 2, 3, 1, 2, 3, 4),
-                   numeric_vcov_matrix_col_label = c(1, 2, 2, 3, 3, 3, 4, 4, 4, 4))
+  expected <- list(
+    numeric_vcov_matrix_row_label = c(1, 1, 2, 1, 2, 3, 1, 2, 3, 4),
+    numeric_vcov_matrix_col_label = c(1, 2, 2, 3, 3, 3, 4, 4, 4, 4)
+  )
   expect_identical(result, expected)
 })
