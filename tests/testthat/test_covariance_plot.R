218cff053cbf432ebd433f1862f149ab8b859243
juliadedic1 <julia.dedic@roche.com>
2022-06-27 16:46:57 -0400

    edits based on comments

--- tests/testthat/test_covariance_plot.R
+++ tests/testthat/test_covariance_plot.R
@@ -10,14 +10,12 @@ test_that("h_get_timepoint_vars works as expected with string specified", {
 })
 
 test_that("h_get_timepoint_vars returns error when string not specified", {
-  data("mmrm_test_data")
   data_wide <- dcast(mmrm_test_data, USUBJID + ARMCD ~ AVISIT, value.var = "FEV1")
   data_cov <- cov(data_wide[, 3:ncol(data_wide)], use = "pairwise.complete.obs", method = "pearson")
   expect_error(h_get_timepoint_vars(vcov_matrix = data_cov))
 })
 
 test_that("h_get_timepoint_vars works as expected with string argument not needed", {
-  data("mmrm_test_data")
   data_wide <- dcast(mmrm_test_data, USUBJID + ARMCD ~ AVISIT, value.var = "FEV1")
   data_cov <- cov(data_wide[, 3:ncol(data_wide)], use = "pairwise.complete.obs", method = "pearson")
   row.names(data_cov) <- c(1, 2, 3, 4)
