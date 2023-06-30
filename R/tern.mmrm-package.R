7381b1e25280d834cd9710ad0373e8b82edf2327
m7pr <marcin.kosinski.mk1@roche.com>
2023-06-30 13:03:09 +0200

    revisit the content of WORDLIST

--- R/tern.mmrm-package.R
+++ R/tern.mmrm-package.R
@@ -22,8 +22,8 @@ NULL
 #'
 #' @description `r lifecycle::badge("stable")`
 #'
-#' Measurements of FEV1 (forced expired volume in one second) is a measure of how quickly the lungs can be emptied.
-#' Low levels of FEV1 may indicate chronic obstructive pulmonary disease (COPD).
+#' Measurements of `FEV1` (forced expired volume in one second) is a measure of how quickly the lungs can be emptied.
+#' Low levels of `FEV1` may indicate chronic obstructive pulmonary disease (`COPD`).
 #' @format A `tibble` with 800 rows and 7 variables:
 #'
 #'   - `USUBJID`: price, in US dollars.
@@ -31,6 +31,6 @@ NULL
 #'   - `ARMCD`: treatment, `TRT` or `PBO`.
 #'   - `RACE`: 3-category race.
 #'   - `SEX`: sex.
-#'   - `FEV1_BL`: FEV1 at baseline (%).
-#'   - `FEV1`: FEV1 at study visits.
+#'   - `FEV1_BL`: `FEV1` at baseline (%).
+#'   - `FEV1`: `FEV1` at study visits.
 "mmrm_test_data"
