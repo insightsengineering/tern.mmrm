055fb72a930e4e0fc55bfd48e786c45ebb1c7e1c
shajoezhu <sha.joe.zhu@gmail.com>
2024-08-28 13:27:46 +0800

    rm vdiffr from testhtat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,16 +1,13 @@
 pkg_name <- "tern.mmrm"
-if (requireNamespace("testthat", quietly = TRUE)) {
-  library(testthat)
-  library(vdiffr)
-  is_on_ci <- isTRUE(as.logical(Sys.getenv("CI")))
-  if (is_on_ci) {
-    reporter <- MultiReporter$new(list(
-      CheckReporter$new()
-    ))
-    test_results <- test_check(pkg_name, reporter = reporter)
-    saveRDS(test_results, "unit_testing_results.rds")
-  } else {
-    reporter <- ParallelProgressReporter$new()
-    test_check(pkg_name, reporter = reporter)
-  }
+library(testthat)
+is_on_ci <- isTRUE(as.logical(Sys.getenv("CI")))
+if (is_on_ci) {
+  reporter <- MultiReporter$new(list(
+    CheckReporter$new()
+  ))
+  test_results <- test_check(pkg_name, reporter = reporter)
+  saveRDS(test_results, "unit_testing_results.rds")
+} else {
+  reporter <- ParallelProgressReporter$new()
+  test_check(pkg_name, reporter = reporter)
 }
